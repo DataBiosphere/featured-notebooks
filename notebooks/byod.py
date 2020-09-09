89ec44c0cef8b0361876bda4fe1bc89fb69d7d82
Brian Hannafious <bhannafi@ucsc.edu>
2020-09-09 11:32:42 -0700

    Introduce the BYOD featured notebook.
    This notebook includes application logic to join Terra data
    tables over a column.

---
+++ notebooks/byod.py
@@ -0,0 +1,224 @@
+# publish to: "terra-notebook-utils-tests" "test-byod"
+import os
+import callysto
+
+# Mock the notebook environment
+os.environ['WORKSPACE_NAME'] = "terra-notebook-utils-tests"
+os.environ['WORKSPACE_BUCKET'] = "gs://fc-9169fcd1-92ce-4d60-9d2d-d19fd326ff10"
+os.environ['GOOGLE_PROJECT'] = "firecloud-cgl"
+
+with callysto.Cell("markdown"):
+    """
+    # Bring your own data to your Terra workspace and organize it in a data table
+    """
+
+with callysto.Cell("markdown"):
+    """
+    If you are planning to upload many files to your Terra workspace, we recommend you organize your data into a Terra
+    data table. This is especially helpful when you plan to run workflows with this data because you can avoid pasting
+    in the "gs://" links to every file and instead use Terra's helpful UI features.
+
+
+    In this example, we introduce tools to help you:
+
+    1. Programmatically upload data from your local machine to your Terra workspace using `gsutil cp`.
+
+    2. Programmatically generate a data table that conatins your samples CRAM and CRAI files. The end result will look
+       like this table:
+
+    | sample_id | cram       | crai      |
+    | --------- | ---------  | --------- |
+    | NWD1      | NWD1.cram  | NWD1.crai |
+    | NWD2      | NWD2.cram  | NWD2.crai |
+    """
+
+with callysto.Cell("python"):
+    #install necessary libraries
+    #%pip install --upgrade --no-cache-dir terra-notebook-utils
+    #%pip install --upgrade --no-cache-dir gs-chunked-io
+    pass
+
+with callysto.Cell("python"):
+    import io
+    import os
+    from uuid import uuid4
+    from firecloud import fiss
+    from collections import defaultdict
+    from typing import Any, List, Set, Dict, Iterable
+
+    google_project = os.environ['GOOGLE_PROJECT']
+    workspace = os.environ['WORKSPACE_NAME']
+
+    def upload_data_table(tsv):
+        resp = fiss.fapi.upload_entities(google_project, workspace, tsv, model="flexible")
+        resp.raise_for_status()
+
+    def upload_rows(table: str, rows: List[Dict[str, Any]]):
+        assert rows
+        columns = sorted(rows[0].keys())
+        tsv_data = "\t".join([f"{table}_id", *columns])
+        for i, row in enumerate(rows):
+            tsv_data += os.linesep + "\t".join([f"{i}", *[row[c] for c in columns]])
+        upload_data_table(tsv_data)
+
+    def upload_columns(table: str, columns: Dict[str, List[Any]]):
+        column_headers = sorted(columns.keys())
+        number_of_rows = len(columns[set(columns.keys()).pop()])
+        tsv_data = "\t".join([f"{table}_id", *column_headers])
+        for i in range(number_of_rows):
+            tsv_data += os.linesep + "\t".join([f"{i}", *[columns[h][i] for h in column_headers]])
+        upload_data_table(tsv_data)
+
+    def iter_ents(table: str):
+        resp = fiss.fapi.get_entities(google_project, workspace, table)
+        resp.raise_for_status()
+        for item in resp.json():
+            yield item
+
+    def iter_rows(table: str):
+        for item in iter_ents(table):
+            yield item['attributes']
+
+    def get_columns(table: str) -> Dict[str, List[Any]]:
+        columns = defaultdict(list)
+        for row in iter_rows(table):
+            for key, val in row.items():
+                columns[key].append(val)
+        return dict(columns)
+
+    def delete_table(table: str):
+        rows_to_delete = [dict(entityType=e['entityType'], entityName=e['name'])
+                          for e in iter_ents(table)]
+        resp = fiss.fapi.delete_entities(google_project, workspace, rows_to_delete)
+        resp.raise_for_status()
+
+    def get_keyed_rows(table_name: str, key_column: str) -> Dict[str, Dict[str, Any]]:
+        keyed_rows = dict()
+        for row in iter_rows(table_name):
+            key = row[key_column]
+            assert key not in keyed_rows
+            keyed_rows[key] = row
+            del keyed_rows[key][key_column]
+        return keyed_rows
+
+    def keyed_row_columns(keyed_rows: Dict[str, Any]) -> Set[str]:
+        if keyed_rows:
+            random_key = set(keyed_rows.keys()).pop()
+            return set(keyed_rows[random_key].keys())
+        else:
+            return set()
+
+    def join_keyed_rows(keyed_rows_a: Dict[str, Any], keyed_rows_b: Dict[str, Any]) -> Dict[str, Any]:
+        assert not keyed_row_columns(keyed_rows_a).intersection(keyed_row_columns(keyed_rows_b))
+        common_keys = set(keyed_rows_a.keys()).intersection(set(keyed_rows_b.keys()))
+        return {k: dict(**keyed_rows_a[k], **keyed_rows_b[k])
+                for k in common_keys}
+
+    def join_data_tables(new_table: str, tables_to_join: list, join_column: str):
+        keyed_rows = get_keyed_rows(tables_to_join[0], join_column)
+        for table_name in tables_to_join[1:]:
+            keyed_rows = join_keyed_rows(keyed_rows, get_keyed_rows(table_name, join_column))
+        upload_rows(new_table, [{join_column: k, **row} for k, row in keyed_rows.items()])
+
+    def create_cram_crai_table(table: str, listing: Iterable[str]):
+        crams = dict()
+        crais = dict()
+        for key in listing:
+            _, filename = key.rsplit("/", 1)
+            sample, ext = filename.split(".", 1)
+            if "cram" == ext:
+                crams[sample] = key
+            elif "crai" == ext:
+                crais[sample] = key
+            else:
+                continue
+        samples = sorted(crams.keys())
+        upload_columns(table, dict(sample=samples,
+                                   cram=[crams[s] for s in samples],
+                                   crai=[crais[s] for s in samples]))
+
+with callysto.Cell("markdown"):
+    """
+    ## Install gsutil on your local computer using option 2 instructions in this [document](https://support.terra.bio/hc/en-us/articles/360024056512-Uploading-to-a-workspace-Google-bucket).
+    """
+
+with callysto.Cell("markdown"):
+    """
+    ## Organize your upload to Terra
+    Using instructions from the document above, you will use the gsutil tool to upload data from your local computer to
+    your Terra workspace. To effectively use the example here, we suggest you upload the data with the suggestions
+    below.
+    """
+
+with callysto.Cell("markdown"):
+    """
+    ### Find the path to this workspace bucket
+    """
+
+with callysto.Cell("markdown"):
+    """
+    Using the os package, you can print your workspace bucket path.
+    """
+
+with callysto.Cell("python"):
+    bucket = os.environ["WORKSPACE_BUCKET"]
+    print(bucket)
+
+with callysto.Cell("markdown"):
+    """
+    ### Add a prefix to your bucket path to organize your data
+    In this example, we add the prefix 'test-crai-cram'. In the terminal of your computer, you will call something like:
+
+    `gsutil cp /Users/Documents/Example.cram gs://your_bucket_info/test-crai-cram/`
+    """
+
+with callysto.Cell("markdown"):
+    """
+    ## Preview the data in your workspace bucket
+    """
+
+with callysto.Cell("python"):
+    #!gsutil ls {bucket}
+    pass
+
+with callysto.Cell("markdown"):
+    """
+    # Generate a data table that links to the data in your workspace bucket
+    """
+
+################################################ TESTS ################################################ noqa
+delete_table("test_cram_crai_table")
+listing = list()
+for i in range(5):
+    listing.append(f"gs://some-bucket/some-pfx/sample_id_{i}.cram")
+    listing.append(f"gs://some-bucket/some-pfx/sample_id_{i}.crai")
+create_cram_crai_table("test_cram_crai_table", listing)
+keyed_rows = get_keyed_rows("test_cram_crai_table", "sample")
+for i in range(5):
+    assert keyed_rows[f'sample_id_{i}'] == dict(cram=f"gs://some-bucket/some-pfx/sample_id_{i}.cram",
+                                                crai=f"gs://some-bucket/some-pfx/sample_id_{i}.crai")
+
+delete_table("test_metadata_table_a")
+test_metadata_table_a_columns = dict(sample=["sample_id_1", "sample_id_2", "sample_id_3", "sample_id_4"],
+                                     firstname=["a", "b", "c", "d"],
+                                     birthday=["e", "f", "g", "h"])
+upload_columns("test_metadata_table_a", test_metadata_table_a_columns)
+
+delete_table("test_metadata_table_b")
+test_metadata_table_b_columns = dict(sample=["sample_id_1", "sample_id_2", "sample_id_3", "sample_id_5"],
+                                     alpha=["1", "2", "3", "4"],
+                                     beta=["5", "6", "7", "8"])
+upload_columns("test_metadata_table_b", test_metadata_table_b_columns)
+
+delete_table("test_joined_table")
+join_data_tables("test_joined_table", ["test_cram_crai_table", "test_metadata_table_a", "test_metadata_table_b"], "sample")
+keyed_rows = get_keyed_rows("test_joined_table", "sample")
+assert set(keyed_rows.keys()) == {"sample_id_1", "sample_id_2", "sample_id_3"}
+for i in [1, 2, 3]:
+    expected_row = dict(cram=f"gs://some-bucket/some-pfx/sample_id_{i}.cram",
+                        crai=f"gs://some-bucket/some-pfx/sample_id_{i}.crai",
+                        firstname=test_metadata_table_a_columns['firstname'][i - 1],
+                        birthday=test_metadata_table_a_columns['birthday'][i - 1],
+                        alpha=test_metadata_table_b_columns['alpha'][i - 1],
+                        beta=test_metadata_table_b_columns['beta'][i - 1])
+    assert expected_row == keyed_rows[f"sample_id_{i}"]
