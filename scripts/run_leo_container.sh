4cc7b6fb6f2ce4151601020b42221fa1fba91ebf
Brian Hannafious <32105697+xbrianh@users.noreply.github.com>
2020-09-25 10:57:51 -0600

    Redirect docker pull output to stderr and display (#26)

--- scripts/run_leo_container.sh
+++ scripts/run_leo_container.sh
@@ -11,8 +11,8 @@ CONTAINER_NAME="leo-container"
 CONTAINER_REPO_ROOT="$(basename ${BDCAT_NOTEBOOKS_HOME})"
 wid=$(docker ps -a --latest  --filter "status=running" --filter "name=${CONTAINER_NAME}" --format="{{.ID}}")
 if [[ -z $wid ]]; then
-	# start new container
-    docker pull ${IMAGE_NAME} > /dev/null 2>&1
+    # start new container
+    docker pull ${IMAGE_NAME} 1>&2
     wid=$(docker run \
           --mount type=bind,source=${BDCAT_NOTEBOOKS_HOME},target=/home/jupyter-user/${CONTAINER_REPO_ROOT} \
           -v ~/.config:/home/jupyter-user/.config \
