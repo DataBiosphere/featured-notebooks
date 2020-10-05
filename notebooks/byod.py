5e064d5f649709a0a425a431fc08d63f1b20043e
DailyDreaming <lblauvel@ucsc.edu>
2020-10-05 14:32:25 -0400

    Imports.

--- notebooks/byod.py
+++ notebooks/byod.py
@@ -150,6 +150,7 @@ with callysto.Cell("markdown"):
 with callysto.Cell("python"):
     bucket = os.environ["WORKSPACE_BUCKET"]
     print(bucket)
+
 with callysto.Cell("markdown"):
     """
     ### Add a prefix to your bucket path to organize your data
@@ -161,6 +162,7 @@ with callysto.Cell("markdown"):
     """
 with callysto.Cell("python"):
     subdirectory = "my-crams"
+
 with callysto.Cell("markdown"):
     """
     #### A technical note on subdirectories in Google Cloud
@@ -237,8 +239,10 @@ with callysto.Cell("markdown"):
     """
 
 with callysto.Cell("python"):
+    os.environ['GOOGLE_PROJECT'] = "firecloud-cgl"
     listing = [key for key in gs.list_bucket(subdirectory)]
     create_cram_crai_table("my-table-name", listing)
+
 with callysto.Cell("markdown"):
     """
     Now, go check the data section of your workspace. You should see a data table with the name you have given it,
