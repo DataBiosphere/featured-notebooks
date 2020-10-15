ca8b336ee56de2ea2a840c09708fe72c8f94cee5
Brian Hannafious <bhannafi@ucsc.edu>
2020-10-15 10:13:43 -0700

    Fix scripts/publish.sh name parser

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -20,7 +20,7 @@ generated_jupyter_notebook=$2
 tnu=$(which tnu || find / -iname "tnu" -executable 2>/dev/null || :)
 publish_directive=$(head -n 1 $source_callysto_notebook)
 workspace=$(echo -n ${publish_directive} | cut -d '"' -f2)
-notebook_name=$(echo -n ${publish_directive} | cut -d '"' -f3- | sed 's/"//g')
+notebook_name=$(echo -n ${publish_directive} | cut -d '"' -f4)
 bucket=$(${tnu} workspace get-bucket --workspace "${workspace}")
 dest="gs://${bucket}/notebooks/${notebook_name}.ipynb"
 if [[ -f $generated_jupyter_notebook ]]; then
