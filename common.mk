6a8189301249b7777ea1931652f4d56bfa67b3a5
Brian Hannafious <32105697+xbrianh@users.noreply.github.com>
2020-10-26 13:59:46 -0600

    Require gsutil (#58)

--- common.mk
+++ common.mk
@@ -1,3 +1,7 @@
 ifndef BDCAT_NOTEBOOKS_HOME
 $(error Please run "source environment" in the bdcate_notebooks repo root directory before running make commands)
 endif
+
+ifeq ($(shell which gsutil),)
+$(error Please install gsutil by following instructions at "https://cloud.google.com/storage/docs/gsutil_install")
+endif
