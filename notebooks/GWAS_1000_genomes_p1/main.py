b0cac028f9ddaf844ad26ff1c45d9038dfc0b2dd
aofarrel <aofarrel@ucsc.edu>
2020-11-30 16:50:49 -0800

    lint fixes

--- notebooks/GWAS_1000_genomes_p1/main.py
+++ notebooks/GWAS_1000_genomes_p1/main.py
@@ -4,20 +4,27 @@
 # Notebook author: Beth Sheets
 # Herzog version: Ash O'Farrell
 
+import os
 import herzog
+
+# Mock the environment
+# UPDATE TO A TEST WORKSPACE
+#os.environ['WORKSPACE_NAME'] = "terra-notebook-utils-tests"
+#os.environ['WORKSPACE_BUCKET'] = "gs://fc-9169fcd1-92ce-4d60-9d2d-d19fd326ff10"
+#os.environ['GOOGLE_PROJECT'] = "firecloud-cgl"
 
 with herzog.Cell("markdown"):
     """
     # Notebook Overview
-    
+
     This notebook consolidates the phentotypic data in tables imported to Terra from Gen3 into one consolidated table of metadata with familiar subject IDs for downstream analysis. It uses functions defined in the terra-util notebook, nd can be adapted to consolidate different metadata. I
 
     # Set up your notebook
     ----
-    
+
     ## Set runtime values
-    If you are opening this notebook for the first time, and you did not edit your runtime settings before starting it, you will now need to change your settings. Click on the gear icon in the upper right to edit your Notebook Runtime. Set the values as specified below: 
-    
+    If you are opening this notebook for the first time, and you did not edit your runtime settings before starting it, you will now need to change your settings. Click on the gear icon in the upper right to edit your Notebook Runtime. Set the values as specified below:
+
     | Option                    | Values                                        |
     |---------------------------|-----------------------------------------------|
     | Environment               | Default (GATK 4.1.4.1, Python 3.7.7, R 4.0.3) |
@@ -27,10 +34,10 @@ with herzog.Cell("markdown"):
     | Compute type              | Standard VM                                   |
     | Persistent disk size (GB) | 50                                            |
 
-    Click the "Replace" button when you are done, and Terra will begin to create a new runtime with your settings. When it is finished, it will pop up asking you to apply the new settings.  
-    
-    ## Check kernel type  
-    
+    Click the "Replace" button when you are done, and Terra will begin to create a new runtime with your settings. When it is finished, it will pop up asking you to apply the new settings.
+
+    ## Check kernel type
+
     A kernel is a _computational engine_ that executes the code in the notebook. You can think of it as defining the programming language. For this notebook, we'll use a `Python 3` kernel. In the upper right corner of the notebook, just under the Notebook Runtime, it should say `Python 3`. If it doesn't, you can switch it by navigating to the Kernel menu and selecting `Change kernel`.
 
     ## Install packages
@@ -54,7 +61,9 @@ with herzog.Cell("python"):
     import numpy as np
     import seaborn as sns
 with herzog.Cell("markdown"):
+    """
     ## Define filepaths and environmental variables
+    """
 
 with herzog.Cell("python"):
     PROJECT = os.environ['GOOGLE_PROJECT']
@@ -79,7 +88,7 @@ with herzog.Cell("python"):
     # Take a look at all the entities (tables) in the workspace
     ent_types = fiss.fapi.list_entity_types(PROJECT, WORKSPACE).json()
     for t in ent_types.keys():
-        print (t, "count:", ent_types[t]['count'])
+        print(t, "count:", ent_types[t]['count'])
 with herzog.Cell("python"):
     # Set the name for your consolidated table
     consolidated_table_name = "consolidated_metadata"
@@ -93,4 +102,4 @@ with herzog.Cell("markdown"):
 
 with herzog.Cell("python"):
     # Consolidate the phenotypic data using the function defined in the terra-util notebook
-    consolidate_gen3_pheno_tables(PROJECT, WORKSPACE, consolidated_table_name)
+    consolidate_gen3_pheno_tables(PROJECT, WORKSPACE, consolidated_table_name)  #type: ignore  # noqa
