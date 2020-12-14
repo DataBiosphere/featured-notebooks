444da99c287056fd49b3de2b761b8d94ca10068d
aofarrel <aofarrel@ucsc.edu>
2020-12-14 13:23:12 -0800

    mock tests for first notebook

--- notebooks/GWAS_blood_pressure_p1/main.py
+++ notebooks/GWAS_blood_pressure_p1/main.py
@@ -8,6 +8,10 @@
 
 import os
 import herzog
+from unittest import mock
+
+file_name = "CCDG_13607_B01_GRM_WGS_2019-02-19_chr22.recalibrated_variants.annotated.vcf.gz" # test fixture
+drs_url = "drs://dg.4503:dg.4503/dbf99b00-414c-49b7-b062-140350acf379" # test fixture
 
 from unittest import mock
 from terra_notebook_utils import table, drs
