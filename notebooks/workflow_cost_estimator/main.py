3ec62958dcfebdf6416ca9bcd315402db250491a
aofarrel <aofarrel@ucsc.edu>
2020-12-16 16:31:44 -0800

    change submission id and where this goes

--- notebooks/workflow_cost_estimator/main.py
+++ notebooks/workflow_cost_estimator/main.py
@@ -69,7 +69,7 @@ submission_id = "7d4d4bbd-6d3a-4e8f-848d-3992f5bd8e33"
 # TODO: workflow metadata expires after 40 days, which will cause this test to break. Is there a better way?
 
 with herzog.Cell("python"):
-    # submission_id = "b25c93e8-41ad-4980-b63c-46963b0402bc"  # Uncomment and insert your submission id here
+    submission_id = "52290aa5-7ac4-4ac0-8087-e7e56f0c0e1e"
     report = pd.DataFrame()
     for shard_info in cost_for_submission(submission_id):
         shard_info['duration'] /= 3600
